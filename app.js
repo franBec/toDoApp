d6490dccac7e427f54e02c1eb367cca173d20a01
Bec <franbecvort@gmail.com>
2021-09-08 00:41:37 -0300

    some files names changes, not including reeact, decided to make toDoApp2

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ const app = express()
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 
-//host connection
-const PORT = process.env.PORT || 3000
-
 //database connection
 const mongoose = require('mongoose')
 const uri=`○mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.z7ncs.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`
@@ -20,22 +17,25 @@ mongoose.connect(uri,connectConfigObject)
 .then(()=>console.log("Connected to database!"))
 .catch(e => console.log(e))
 
-//template engine
+//template engine, probably replaced by react
 app.set('view engine', 'ejs')
 app.set('views', __dirname+'/views')
 
-//static route
+//static route, not sure what is doing ¿?
 app.use(express.static(__dirname+"/public"))
 
-//middleware: routing
-app.use('/', require('./router/routesConfig'))
-app.use('/tasks', require('./router/tasksApi'))
+//middleware: routing to pages
+app.use('/', require('./routing/routingPaths'))
+app.use('/tasks', require('./routing/tasksApi'))
 
 //middleware: 404 response
 app.use((req, res, next)=>{
     res.status(404).render("404", {})
 })
 
+
+//host connection
+const PORT = process.env.PORT || 3001
 app.listen(PORT, ()=>{
     console.log("Server is working at: ", PORT)
 })
